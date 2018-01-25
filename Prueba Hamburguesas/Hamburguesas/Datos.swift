7137a8ba1eae89b4af7bb780bfadc4b879c78232
Ramon <ramondavila@gmail.com>
2018-01-26 00:19:24 +0100

    My first commit

---
+++ Prueba Hamburguesas/Hamburguesas/Datos.swift
@@ -0,0 +1,21 @@
+//
+//  Datos.swift
+//  Hamburguesas
+//
+//  Created by Ramón Dávila Ruiz on 18/1/18.
+//  Copyright © 2018 Ramón Dávila Ruiz. All rights reserved.
+//
+
+import Foundation
+
+class ColeccionDePaises{
+    
+    var paises: [String] = ["España", "Portugal", "UK", "Francia", "Italia", "Marruecos", "México", "Cuba", "USA", "Canada", "Rusia", "China", "Austria", "Polonia", "Bélgica", "Alemania", "Hungría", "Rumanía", "Grecia", "Turquía"]
+    
+}
+
+class ColeccionDeHamburguesa{
+    
+    var hamburguesas: [String] = ["Clásica", "Tejana", "Cajún", "Andaluza", "Catalana", "Pollo picante", "Japonesa", "Vegana", "Kobe", "Pescado", "McRoyale", "Big King", "McPollo", "Cheeseburguer", "Doble buey", "Doble bacon", "Whopper", "Big King", "Doble whopper", "Gourmet"]
+    
+}
